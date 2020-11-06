eea01aafa347ad3840bf9768707e996bccc07c95
introbeginner <intronovice@gmail.com>
2020-11-06 15:13:25 +0900

    init first, alalytics later

--- src/firebase.js
+++ src/firebase.js
@@ -14,6 +14,7 @@ import "firebase/analytics";
     measurementId: "G-RLV5TDSZTY"
   };
   // Initialize Firebase
+  export default firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
-  export default firebase.initializeApp(firebaseConfig);
+  
