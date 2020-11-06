b05a61413093dda0b7d5599bcdc46d0cb028cabe
introbeginner <intronovice@gmail.com>
2020-11-06 15:29:59 +0900

    hiding git

--- src/firebase.js
+++ src/firebase.js
@@ -4,17 +4,15 @@ import "firebase/analytics";
 // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyDWwnipH9hT3xOkJkwOTeanKKP2ohdgm2w",
-    authDomain: "nwitter-b02e7.firebaseapp.com",
-    databaseURL: "https://nwitter-b02e7.firebaseio.com",
-    projectId: "nwitter-b02e7",
-    storageBucket: "nwitter-b02e7.appspot.com",
-    messagingSenderId: "1011602635404",
-    appId: "1:1011602635404:web:2a60e5319e276d94f7d9a9",
-    measurementId: "G-RLV5TDSZTY"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURE_ID
   };
   // Initialize Firebase
   export default firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-
-  
